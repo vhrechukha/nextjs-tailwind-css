91e030ba19679bc293d583efe5628b1a40fc4ba3
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-08-05 21:58:28 +0200

    Added form layout

--- src/app/form/components/EmployeeForm.tsx
+++ src/app/form/components/EmployeeForm.tsx
@@ -34,7 +34,28 @@ const EmployeeForm = () => {
           </Form.Control>
         </Form.Field>
       </Card>
-      <Card>
+      <Card title="Employee Data">
+        <Form.Field className="grid mb-[10px]" name="question">
+          <div className="flex items-baseline justify-between">
+            <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
+              Question
+            </Form.Label>
+            <Form.Message
+              className="text-[13px] text-white opacity-[0.8]"
+              match="valueMissing"
+            >
+              Please enter a question
+            </Form.Message>
+          </div>
+          <Form.Control asChild>
+            <textarea
+              className="box-border w-full bg-blackA5 shadow-blackA9 inline-flex appearance-none items-center justify-center rounded-[4px] p-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9 resize-none"
+              required
+            />
+          </Form.Control>
+        </Form.Field>
+      </Card>
+      <Card title="Address Details">
         <Form.Field className="grid mb-[10px]" name="question">
           <div className="flex items-baseline justify-between">
             <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
@@ -56,8 +77,11 @@ const EmployeeForm = () => {
         </Form.Field>
       </Card>
       <Form.Submit asChild>
-        <button className="box-border w-full text-violet11 shadow-blackA7 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-          Post question
+        <button
+          className="mt-[10px] box-border inline-flex h-[35px] w-full items-center justify-center rounded-[4px] bg-blue-600 px-[15px] font-medium leading-none text-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-blue-500 focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
+          type="submit"
+        >
+          Submit
         </button>
       </Form.Submit>
     </Form.Root>
