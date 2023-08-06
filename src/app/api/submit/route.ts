c44f7dc8cd143bfeb98060156d0e11722a3e7fe4
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-08-06 17:11:54 +0200

    Added logs in mocked request

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,5 +1,10 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: Request) {
+  // TODO: write form data in DB
+  const formData = await request.json();
+
+  console.log("Form submitted, data: ", formData);
+
   return NextResponse.json({ success: true });
 }
